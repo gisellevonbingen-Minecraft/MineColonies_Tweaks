7c49e78f79226ab383bd0b05ddb046aad7f6c556
gisellevonbingen <gisellevonbingen@gmail.com>
2024-04-08 19:57:46 +0900

    Added config of chance about generate empty colonies

---
+++ src/main/java/steve_gall/minecolonies_tweaks/common/config/WorldGenConfig.java
@@ -0,0 +1,17 @@
+package steve_gall.minecolonies_tweaks.common.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.DoubleValue;
+
+public class WorldGenConfig
+{
+	public final DoubleValue emptyColoniesGenerationChance;
+	public final DoubleValue emptyNetherColoniesGenerationChance;
+
+	public WorldGenConfig(ForgeConfigSpec.Builder builder)
+	{
+		this.emptyColoniesGenerationChance = builder.defineInRange("emptyColoniesGenerationChance", 1.0D, 0.0D, 1.0D);
+		this.emptyNetherColoniesGenerationChance = builder.defineInRange("emptyNetherColoniesGenerationChance", 1.0D, 0.0D, 1.0D);
+	}
+
+}
