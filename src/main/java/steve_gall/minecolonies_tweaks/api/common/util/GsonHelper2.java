cf1899e24fba1beeca393ace67c693fb61219310
gisellevonbingen <gisellevonbingen@gmail.com>
2024-04-10 22:44:36 +0900

    Update CustomToolTypeData.java

---
+++ src/main/java/steve_gall/minecolonies_tweaks/api/common/util/GsonHelper2.java
@@ -0,0 +1,30 @@
+package steve_gall.minecolonies_tweaks.api.common.util;
+
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.google.gson.JsonObject;
+
+public class GsonHelper2
+{
+	@NotNull
+	public static <T> Optional<T> of(@NotNull JsonObject json, @NotNull String memberName, @NotNull BiFunction<JsonObject, String, T> function)
+	{
+		return json.has(memberName) ? Optional.of(function.apply(json, memberName)) : Optional.empty();
+	}
+
+	@NotNull
+	public static <T> void ifPresent(@NotNull String memberName, @NotNull Optional<T> value, @NotNull BiConsumer<String, T> consumer)
+	{
+		value.ifPresent(v -> consumer.accept(memberName, v));
+	}
+
+	private GsonHelper2()
+	{
+
+	}
+
+}
