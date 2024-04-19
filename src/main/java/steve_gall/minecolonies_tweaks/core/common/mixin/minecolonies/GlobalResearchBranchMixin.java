6f184b40b2d2f0cc1d0cddef6f010902480c3ab6
gisellevonbingen <gisellevonbingen@gmail.com>
2024-04-20 00:58:29 +0900

    Added research speed config

---
+++ src/main/java/steve_gall/minecolonies_tweaks/core/common/mixin/minecolonies/GlobalResearchBranchMixin.java
@@ -0,0 +1,32 @@
+package steve_gall.minecolonies_tweaks.core.common.mixin.minecolonies;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.minecolonies.core.research.GlobalResearchBranch;
+
+import steve_gall.minecolonies_tweaks.core.common.config.MineColoniesTweaksConfigServer;
+
+@Mixin(value = GlobalResearchBranch.class, remap = false)
+public class GlobalResearchBranchMixin
+{
+	@Inject(method = "getBaseTime", at = @At(value = "RETURN"), cancellable = true)
+	private void getBaseTime(int depth, CallbackInfoReturnable<Integer> cir)
+	{
+		var original = cir.getReturnValue().doubleValue();
+		var speed = MineColoniesTweaksConfigServer.INSTANCE.researches.speed.get().doubleValue();
+
+		if (speed == 0.0D)
+		{
+			cir.setReturnValue(Integer.MAX_VALUE);
+		}
+		else
+		{
+			cir.setReturnValue((int) (original / speed));
+		}
+
+	}
+
+}
