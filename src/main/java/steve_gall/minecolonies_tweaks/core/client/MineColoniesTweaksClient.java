174bee8445d4b3dd81f7b96df184c628bc2098f1
Giselle <gisellevonbingen@gmail.com>
2024-04-24 16:32:55 +0900

    Added Addition pane

--- src/main/java/steve_gall/minecolonies_tweaks/core/client/MineColoniesTweaksClient.java
+++ src/main/java/steve_gall/minecolonies_tweaks/core/client/MineColoniesTweaksClient.java
@@ -1,7 +1,11 @@
 package steve_gall.minecolonies_tweaks.core.client;
 
+import com.ldtteam.blockui.Loader;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import steve_gall.minecolonies_tweaks.core.client.view.Addition;
+import steve_gall.minecolonies_tweaks.core.common.MineColoniesTweaks;
 
 public class MineColoniesTweaksClient
 {
@@ -9,6 +13,8 @@ public class MineColoniesTweaksClient
 	{
 		var fml_bus = FMLJavaModLoadingContext.get().getModEventBus();
 		var forge_bus = MinecraftForge.EVENT_BUS;
+
+		Loader.INSTANCE.register(MineColoniesTweaks.rl("addition").toString(), Addition::new);
 	}
 
 }
