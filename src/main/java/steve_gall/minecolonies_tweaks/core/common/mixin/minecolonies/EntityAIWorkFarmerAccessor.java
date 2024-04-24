c15d0eb280a16b9fd787df30b631f41575a00b05
Giselle <gisellevonbingen@gmail.com>
2024-04-24 23:07:39 +0900

    Added farmer configs

---
+++ src/main/java/steve_gall/minecolonies_tweaks/core/common/mixin/minecolonies/EntityAIWorkFarmerAccessor.java
@@ -0,0 +1,23 @@
+package steve_gall.minecolonies_tweaks.core.common.mixin.minecolonies;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import com.minecolonies.core.entity.ai.citizen.farmer.EntityAIWorkFarmer;
+
+@Mixin(value = EntityAIWorkFarmer.class, remap = false)
+public interface EntityAIWorkFarmerAccessor
+{
+	@Accessor("STANDARD_DELAY")
+	static int getStandardDelay()
+	{
+		throw new AssertionError();
+	}
+
+	@Accessor("MAX_BLOCKS_MINED")
+	static int getMaxBlocksMined()
+	{
+		throw new AssertionError();
+	}
+
+}
