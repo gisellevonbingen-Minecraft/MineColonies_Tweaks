a4c70068ede9d570014b5143eb4d224e5a16579f
Giselle <gisellevonbingen@gmail.com>
2024-04-24 23:07:39 +0900

    Added farmer configs

--- src/main/java/steve_gall/minecolonies_tweaks/core/common/config/JobConfig.java
+++ src/main/java/steve_gall/minecolonies_tweaks/core/common/config/JobConfig.java
@@ -8,6 +8,11 @@ import net.minecraftforge.common.ForgeConfigSpec.IntValue;
 import steve_gall.minecolonies_tweaks.core.common.mixin.minecolonies.AbstractEntityAICraftingAccessor;
 import steve_gall.minecolonies_tweaks.core.common.mixin.minecolonies.AbstractEntityAIStructureAccessor;
 import steve_gall.minecolonies_tweaks.core.common.mixin.minecolonies.EntityAIStructureBuilderAccessor;
+import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
+import net.minecraftforge.common.ForgeConfigSpec.DoubleValue;
+import net.minecraftforge.common.ForgeConfigSpec.IntValue;
+import steve_gall.minecolonies_tweaks.core.common.mixin.minecolonies.AbstractEntityAICraftingAccessor;
+import steve_gall.minecolonies_tweaks.core.common.mixin.minecolonies.EntityAIWorkFarmerAccessor;
 import steve_gall.minecolonies_tweaks.core.common.mixin.minecolonies.EntityAIWorkSifterAccessor;
 
 public class JobConfig
@@ -19,6 +24,11 @@ public class JobConfig
 	public final IntValue craftingProgressMultiplier;
 	public final IntValue craftingHittingTime;
 	public final IntValue sifterProgressMultiplier;
+	public final IntValue farmerWorkDelay;
+	public final DoubleValue farmerSkillDivider;
+	public final IntValue farmerActionsDoneUntilDumping;
+	public final BooleanValue farmerPlantAfterHoe;
+	public final BooleanValue farmerPlantAfterHarvest;
 
 	public JobConfig(ForgeConfigSpec.Builder builder)
 	{
@@ -39,6 +49,18 @@ public class JobConfig
 		builder.push("sifter");
 		builder.comment("siftingTicks = progressMultiplier - strengthLevel");
 		this.sifterProgressMultiplier = builder.defineInRange("progressMultiplier", EntityAIWorkSifterAccessor.getMaxLevel(), 0, Integer.MAX_VALUE);
+		builder.pop();
+
+		builder.push("farmer");
+		builder.comment("finalDelay = workDelay - (skillDivider * staminaLevel)");
+		this.farmerWorkDelay = builder.defineInRange("workDelay", EntityAIWorkFarmerAccessor.getStandardDelay(), 0, Integer.MAX_VALUE);
+		this.farmerSkillDivider = builder.defineInRange("farmerSkillDivider", 2.0D, 1.0D, Integer.MAX_VALUE);
+		builder.comment("if havested count reached to this, farmer will go to dump");
+		this.farmerActionsDoneUntilDumping = builder.defineInRange("actionsDoneUntilDumping", EntityAIWorkFarmerAccessor.getMaxBlocksMined(), EntityAIWorkFarmerAccessor.getMaxBlocksMined(), Integer.MAX_VALUE);
+		builder.comment("whether plant seed after hoeing dirt");
+		this.farmerPlantAfterHoe = builder.define("plantAfterHoe", true);
+		builder.comment("whether plant seed after harvest crop");
+		this.farmerPlantAfterHarvest = builder.define("plantAfterHarvest", true);
 		builder.pop();
 	}
 
---
+++ src/main/java/steve_gall/minecolonies_tweaks/core/common/mixin/minecolonies/EntityAIWorkFarmerMixin.java
@@ -0,0 +1,117 @@
+package steve_gall.minecolonies_tweaks.core.common.mixin.minecolonies;
+
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.minecolonies.api.compatibility.Compatibility;
+import com.minecolonies.core.colony.buildings.modules.FieldsModule;
+import com.minecolonies.core.colony.buildings.workerbuildings.BuildingFarmer;
+import com.minecolonies.core.colony.fields.FarmField;
+import com.minecolonies.core.colony.jobs.JobFarmer;
+import com.minecolonies.core.entity.ai.workers.crafting.AbstractEntityAICrafting;
+import com.minecolonies.core.entity.ai.workers.production.agriculture.EntityAIWorkFarmer;
+
+import net.minecraft.core.BlockPos;
+import steve_gall.minecolonies_tweaks.core.common.config.MineColoniesTweaksConfigServer;
+
+@Mixin(value = EntityAIWorkFarmer.class, remap = false)
+public abstract class EntityAIWorkFarmerMixin extends AbstractEntityAICrafting<JobFarmer, BuildingFarmer>
+{
+	@Unique
+	private BlockPos minecolonies_tweaks$workingPosition;
+
+	public EntityAIWorkFarmerMixin(@NotNull JobFarmer job)
+	{
+		super(job);
+	}
+
+	@Shadow
+	private boolean hoeIfAble(BlockPos position, FarmField farmField)
+	{
+		throw new AssertionError();
+	}
+
+	@Shadow
+	private boolean tryToPlant(FarmField farmField, BlockPos position)
+	{
+		throw new AssertionError();
+	}
+
+	@Shadow
+	private boolean harvestIfAble(BlockPos position)
+	{
+		throw new AssertionError();
+	}
+
+	@Inject(method = "hoeIfAble", at = @At(value = "HEAD"), cancellable = true)
+	private void hoeIfAble_HEAD(BlockPos position, FarmField farmField, CallbackInfoReturnable<Boolean> cir)
+	{
+		this.minecolonies_tweaks$workingPosition = position;
+	}
+
+	@Inject(method = "hoeIfAble", at = @At(value = "RETURN"), cancellable = true)
+	private void hoeIfAble_Return(BlockPos position, FarmField farmField, CallbackInfoReturnable<Boolean> cir)
+	{
+		if (MineColoniesTweaksConfigServer.INSTANCE.jobs.farmerPlantAfterHoe.get().booleanValue())
+		{
+			this.tryToPlant(farmField, this.minecolonies_tweaks$workingPosition);
+		}
+
+	}
+
+	@Inject(method = "harvestIfAble", at = @At(value = "HEAD"), cancellable = true)
+	private void harvestIfAble_Head(@NotNull BlockPos position, CallbackInfoReturnable<Boolean> cir)
+	{
+		this.minecolonies_tweaks$workingPosition = position;
+	}
+
+	@Inject(method = "harvestIfAble", at = @At(value = "RETURN"), cancellable = true)
+	private void harvestIfAble_Return(@NotNull BlockPos position, CallbackInfoReturnable<Boolean> cir)
+	{
+		if (cir.getReturnValueZ() && !Compatibility.isPamsInstalled() && position != null)
+		{
+			if (MineColoniesTweaksConfigServer.INSTANCE.jobs.farmerPlantAfterHarvest.get().booleanValue())
+			{
+				if (this.building.getFirstModuleOccurance(FieldsModule.class).getCurrentField() instanceof FarmField farmField)
+				{
+					this.tryToPlant(farmField, this.minecolonies_tweaks$workingPosition);
+				}
+
+			}
+
+		}
+
+	}
+
+	@ModifyConstant(method = "getLevelDelay", constant = @Constant(doubleValue = 40))
+	private double getLevelDelay_standardDelay(double STANDARD_DELAY)
+	{
+		return MineColoniesTweaksConfigServer.INSTANCE.jobs.farmerWorkDelay.get().intValue();
+	}
+
+	@ModifyConstant(method = "getLevelDelay", constant = @Constant(doubleValue = 2.0))
+	private double getLevelDelay_skillDivider(double skillDivider)
+	{
+		return MineColoniesTweaksConfigServer.INSTANCE.jobs.farmerSkillDivider.get().doubleValue();
+	}
+
+	@ModifyConstant(method = "getActionRewardForCraftingSuccess", constant = @Constant(intValue = 64))
+	private int getActionRewardForCraftingSuccess(int MAX_BLOCKS_MINED)
+	{
+		return MineColoniesTweaksConfigServer.INSTANCE.jobs.farmerActionsDoneUntilDumping.get().intValue();
+	}
+
+	@ModifyConstant(method = "getActionsDoneUntilDumping", constant = @Constant(intValue = 64))
+	private int getActionsDoneUntilDumping(int MAX_BLOCKS_MINED)
+	{
+		return MineColoniesTweaksConfigServer.INSTANCE.jobs.farmerActionsDoneUntilDumping.get().intValue();
+	}
+
+}
