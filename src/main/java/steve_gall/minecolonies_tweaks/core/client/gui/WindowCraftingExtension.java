4cf26f6484b032d8f0a8b2a04fabf18e516a52df
Giselle <gisellevonbingen@gmail.com>
2024-05-05 16:16:15 +0900

    Added close button at Crafting Windows

---
+++ src/main/java/steve_gall/minecolonies_tweaks/core/client/gui/WindowCraftingExtension.java
@@ -0,0 +1,10 @@
+package steve_gall.minecolonies_tweaks.core.client.gui;
+
+import net.minecraft.client.gui.screens.Screen;
+
+public interface WindowCraftingExtension
+{
+	void minecolonies_tweaks$setParent(Screen screen);
+
+	Screen minecolonies_tweaks$getParent();
+}
