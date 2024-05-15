b0b76f9716dfd6c0b44ba69550ed1cf565681fcd
Giselle <gisellevonbingen@gmail.com>
2024-05-15 15:46:46 +0900

    Refactor

--- src/main/java/steve_gall/minecolonies_tweaks/core/client/gui/CloseableWindowExtension.java
+++ src/main/java/steve_gall/minecolonies_tweaks/core/client/gui/CloseableWindowExtension.java
@@ -2,6 +2,7 @@ package steve_gall.minecolonies_tweaks.core.client.gui;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import steve_gall.minecolonies_tweaks.core.common.config.MineColoniesTweaksConfigClient;
 
 public interface CloseableWindowExtension
@@ -10,7 +11,42 @@ public interface CloseableWindowExtension
 
 	Screen minecolonies_tweaks$getParent();
 
-	default boolean returnToParent(boolean isEsc)
+	default boolean minecolonies_tweaks$returnOrClose()
+	{
+		if (this instanceof Screen screen)
+		{
+			var closed = false;
+
+			if (screen instanceof AbstractContainerScreen<?> containerScreen)
+			{
+				containerScreen.onClose();
+				closed = true;
+			}
+
+			if (!this.minecolonies_tweaks$showParent(false))
+			{
+				if (!closed)
+				{
+					screen.onClose();
+				}
+
+			}
+
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
+	default boolean minecolonies_tweaks$showParent()
+	{
+		return this.minecolonies_tweaks$showParent(true);
+	}
+
+	default boolean minecolonies_tweaks$showParent(boolean isEsc)
 	{
 		if (isEsc && !MineColoniesTweaksConfigClient.INSTANCE.escToReturn.get().booleanValue())
 		{
@@ -24,7 +60,13 @@ public interface CloseableWindowExtension
 			return false;
 		}
 
-		Minecraft.getInstance().setScreen(parent);
+		var minecraft = Minecraft.getInstance();
+
+		if (minecraft.screen != parent)
+		{
+			minecraft.setScreen(parent);
+		}
+
 		return true;
 	}
 
