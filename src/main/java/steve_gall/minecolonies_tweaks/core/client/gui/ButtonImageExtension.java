fb36652d88e04e94ede0a6b5b3edd6bd9477b1de
Giselle <gisellevonbingen@gmail.com>
2024-05-16 11:14:36 +0900

    Module button can be placed at right side

---
+++ src/main/java/steve_gall/minecolonies_tweaks/core/client/gui/ButtonImageExtension.java
@@ -0,0 +1,8 @@
+package steve_gall.minecolonies_tweaks.core.client.gui;
+
+public interface ButtonImageExtension
+{
+	boolean minecolonies_tweaks$isFlipX();
+
+	void minecolonies_tweaks$setFlipX(boolean flipX);
+}
