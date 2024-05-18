1bb9ca39f0ceb6f04146644951e08b976034d662
Giselle <gisellevonbingen@gmail.com>
2024-05-18 15:41:34 +0900

    Can't accept recipe if module not found

---
+++ src/main/java/steve_gall/minecolonies_tweaks/mixin/common/minecolonies/PublicWorkerCraftingRequestResolverMixin.java
@@ -0,0 +1,38 @@
+package steve_gall.minecolonies_tweaks.mixin.common.minecolonies;
+
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.minecolonies.api.colony.jobs.registry.JobEntry;
+import com.minecolonies.api.colony.requestsystem.location.ILocation;
+import com.minecolonies.api.colony.requestsystem.token.IToken;
+import com.minecolonies.api.crafting.IRecipeStorage;
+import com.minecolonies.core.colony.buildings.AbstractBuilding;
+import com.minecolonies.core.colony.requestsystem.resolvers.PublicWorkerCraftingRequestResolver;
+import com.minecolonies.core.colony.requestsystem.resolvers.core.AbstractCraftingRequestResolver;
+
+import steve_gall.minecolonies_tweaks.api.common.crafting.ICustomizableRecipeStorage;
+import steve_gall.minecolonies_tweaks.core.common.building.BuildingHelper;
+
+@Mixin(value = PublicWorkerCraftingRequestResolver.class, remap = false)
+public abstract class PublicWorkerCraftingRequestResolverMixin extends AbstractCraftingRequestResolver
+{
+	public PublicWorkerCraftingRequestResolverMixin(@NotNull ILocation location, @NotNull IToken<?> token, @NotNull JobEntry entry, boolean isPublicCrafter)
+	{
+		super(location, token, entry, isPublicCrafter);
+	}
+
+	@Inject(method = "canBuildingCraftRecipe", remap = false, at = @At(value = "HEAD"), cancellable = true)
+	private void canBuildingCraftRecipe(AbstractBuilding building, IRecipeStorage recipeStorage, CallbackInfoReturnable<Boolean> cir)
+	{
+		if (recipeStorage instanceof ICustomizableRecipeStorage)
+		{
+			cir.setReturnValue(BuildingHelper.getCraftableModule(building, recipeStorage.getToken(), this.getJobEntry()) != null);
+		}
+
+	}
+
+}
