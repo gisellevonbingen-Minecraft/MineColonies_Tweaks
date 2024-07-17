ac84b9efd00b0d8ee0c4df15abaf993295314ec5
Giselle <gisellevonbingen@gmail.com>
2024-07-17 22:45:57 +0900

    Refacted

--- src/main/java/steve_gall/minecolonies_tweaks/core/common/network/message/BuildingModuleMessage.java
+++ src/main/java/steve_gall/minecolonies_tweaks/core/common/network/message/BuildingModuleMessage.java
@@ -1,49 +1,31 @@
 package steve_gall.minecolonies_tweaks.core.common.network.message;
 
-import com.minecolonies.api.colony.IColonyManager;
 import com.minecolonies.api.colony.buildings.modules.IBuildingModule;
 import com.minecolonies.api.colony.buildings.modules.IBuildingModuleView;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.Level;
+import steve_gall.minecolonies_tweaks.api.common.building.module.ModulePos;
 import steve_gall.minecolonies_tweaks.core.common.network.AbstractMessage;
 
 public abstract class BuildingModuleMessage extends AbstractMessage
 {
-	private final ResourceKey<Level> dimensionId;
-	private final int coloyId;
-	private final BlockPos buildingId;
-	private final int moduleId;
+	private final ModulePos modulePos;
 
 	public BuildingModuleMessage(IBuildingModule module)
 	{
-		var building = module.getBuilding();
-		this.dimensionId = building.getColony().getDimension();
-		this.coloyId = building.getColony().getID();
-		this.buildingId = building.getID();
-		this.moduleId = module.getProducer().getRuntimeID();
+		this.modulePos = new ModulePos(module);
 	}
 
 	public BuildingModuleMessage(IBuildingModuleView module)
 	{
-		var building = module.getBuildingView();
-		this.dimensionId = building.getColony().getDimension();
-		this.coloyId = building.getColony().getID();
-		this.buildingId = building.getID();
-		this.moduleId = module.getProducer().getRuntimeID();
+		this.modulePos = new ModulePos(module);
 	}
 
 	public BuildingModuleMessage(FriendlyByteBuf buffer)
 	{
 		super(buffer);
 
-		this.dimensionId = buffer.readResourceKey(Registries.DIMENSION);
-		this.coloyId = buffer.readInt();
-		this.buildingId = buffer.readBlockPos();
-		this.moduleId = buffer.readInt();
+		this.modulePos = new ModulePos(buffer);
 	}
 
 	@Override
@@ -51,61 +33,12 @@ public abstract class BuildingModuleMessage extends AbstractMessage
 	{
 		super.encode(buffer);
 
-		buffer.writeResourceKey(this.dimensionId);
-		buffer.writeInt(this.coloyId);
-		buffer.writeBlockPos(this.buildingId);
-		buffer.writeInt(this.moduleId);
+		this.modulePos.serializeBuffer(buffer);
 	}
 
-	public IBuildingModule getModule()
+	public ModulePos getModulePos()
 	{
-		var colony = IColonyManager.getInstance().getColonyByDimension(this.coloyId, this.dimensionId);
-
-		if (colony == null)
-		{
-			return null;
-		}
-
-		var building = colony.getBuildingManager().getBuilding(this.buildingId);
-
-		if (building == null)
-		{
-			return null;
-		}
-
-		return building.getModule(this.moduleId);
-	}
-
-	public IBuildingModuleView getModuleView()
-	{
-		var building = IColonyManager.getInstance().getBuildingView(this.dimensionId, this.buildingId);
-
-		if (building == null)
-		{
-			return null;
-		}
-
-		return building.getModuleView(this.moduleId);
-	}
-
-	public ResourceKey<Level> getDimensionId()
-	{
-		return this.dimensionId;
-	}
-
-	public int getColoyId()
-	{
-		return this.coloyId;
-	}
-
-	public BlockPos getBuildingId()
-	{
-		return this.buildingId;
-	}
-
-	public int getModuleId()
-	{
-		return this.moduleId;
+		return this.modulePos;
 	}
 
 }
