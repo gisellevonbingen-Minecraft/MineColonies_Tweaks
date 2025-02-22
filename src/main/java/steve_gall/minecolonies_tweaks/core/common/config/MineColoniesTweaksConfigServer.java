4e4f4f42a8c0fe389cf444f850489cf5a4a1b766
Giselle <gisellevonbingen@gmail.com>
2025-02-22 15:53:58 +0900

    Added config raider disableImmunity

--- src/main/java/steve_gall/minecolonies_tweaks/core/common/config/MineColoniesTweaksConfigServer.java
+++ src/main/java/steve_gall/minecolonies_tweaks/core/common/config/MineColoniesTweaksConfigServer.java
@@ -18,6 +18,7 @@ public class MineColoniesTweaksConfigServer
 	public final FieldConfig fields;
 	public final JobConfig jobs;
 	public final CitizenConfig citizens;
+	public final MonsterConfig monsters;
 
 	public MineColoniesTweaksConfigServer(ForgeConfigSpec.Builder builder)
 	{
@@ -35,6 +36,10 @@ public class MineColoniesTweaksConfigServer
 
 		builder.push("citizens");
 		this.citizens = new CitizenConfig(builder);
+		builder.pop();
+
+		builder.push("monsters");
+		this.monsters = new MonsterConfig(builder);
 		builder.pop();
 	}
 
---
+++ src/main/java/steve_gall/minecolonies_tweaks/mixin/common/minecolonies/AbstractEntityRaiderMobMixin.java
@@ -0,0 +1,65 @@
+package steve_gall.minecolonies_tweaks.mixin.common.minecolonies;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.minecolonies.api.entity.mobs.AbstractEntityRaiderMob;
+
+import steve_gall.minecolonies_tweaks.core.common.config.MineColoniesTweaksConfigServer;
+
+@Mixin(value = AbstractEntityRaiderMob.class, remap = false)
+public abstract class AbstractEntityRaiderMobMixin
+{
+	@Shadow(remap = false)
+	private int envDamageInterval;
+	@Shadow(remap = false)
+	private boolean envDamageImmunity;
+	@Shadow(remap = false)
+	private boolean tempEnvDamageImmunity;
+
+	@Inject(method = "initStatsFor", remap = false, at = @At(value = "HEAD"), cancellable = false)
+	private void initStatsFor(double baseHealth, double difficulty, double baseDamage, CallbackInfo ci)
+	{
+		if (MineColoniesTweaksConfigServer.INSTANCE.monsters.disableImmunity.get())
+		{
+			this.envDamageInterval = 0;
+			this.envDamageImmunity = false;
+			this.tempEnvDamageImmunity = false;
+		}
+
+	}
+
+	@Inject(method = "setEnvDamageInterval", remap = false, at = @At(value = "HEAD"), cancellable = true)
+	private void setEnvDamageInterval(int interval, CallbackInfo ci)
+	{
+		if (interval > 0 && MineColoniesTweaksConfigServer.INSTANCE.monsters.disableImmunity.get())
+		{
+			ci.cancel();
+		}
+
+	}
+
+	@Inject(method = "setEnvDamageImmunity", remap = false, at = @At(value = "HEAD"), cancellable = true)
+	private void setEnvDamageImmunity(boolean immunity, CallbackInfo ci)
+	{
+		if (immunity && MineColoniesTweaksConfigServer.INSTANCE.monsters.disableImmunity.get())
+		{
+			ci.cancel();
+		}
+
+	}
+
+	@Inject(method = "setTempEnvDamageImmunity", remap = false, at = @At(value = "HEAD"), cancellable = true)
+	private void setTempEnvDamageImmunity(boolean immunity, CallbackInfo ci)
+	{
+		if (immunity && MineColoniesTweaksConfigServer.INSTANCE.monsters.disableImmunity.get())
+		{
+			ci.cancel();
+		}
+
+	}
+
+}
