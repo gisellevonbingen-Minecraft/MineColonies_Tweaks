ab3d39ad3de82e5373fcf7e89a1693e5008261b4
Giselle <gisellevonbingen@gmail.com>
2025-03-31 20:08:36 +0900

    WindowColonyMap be closeable

--- src/main/java/steve_gall/minecolonies_tweaks/mixin/client/minecolonies/AbstractWindowSkeletonsMixin.java
+++ src/main/java/steve_gall/minecolonies_tweaks/mixin/client/minecolonies/AbstractWindowSkeletonsMixin.java
@@ -9,11 +9,12 @@ import com.minecolonies.core.client.gui.WindowClipBoard;
 import com.minecolonies.core.client.gui.WindowHireWorker;
 import com.minecolonies.core.client.gui.WindowHutAllInventory;
 import com.minecolonies.core.client.gui.WindowResourceList;
+import com.minecolonies.core.client.gui.map.WindowColonyMap;
 
 import net.minecraft.client.gui.screens.Screen;
 import steve_gall.minecolonies_tweaks.core.client.gui.CloseableWindowExtension;
 
-@Mixin(value = {WindowBuildBuilding.class, WindowHireWorker.class, WindowHutAllInventory.class, WindowAssignCitizen.class, WindowResourceList.class, WindowClipBoard.class}, remap = false)
+@Mixin(value = {WindowBuildBuilding.class, WindowHireWorker.class, WindowHutAllInventory.class, WindowAssignCitizen.class, WindowResourceList.class, WindowClipBoard.class, WindowColonyMap.class}, remap = false)
 public abstract class AbstractWindowSkeletonsMixin implements CloseableWindowExtension
 {
 	@Unique
