493419e2761b2d0f7723e792d70f07a2078ac162
Giselle <gisellevonbingen@gmail.com>
2025-04-05 13:16:59 +0900

    Added MinimumStack to Postbox

--- src/main/java/steve_gall/minecolonies_tweaks/core/common/MineColoniesTweaks.java
+++ src/main/java/steve_gall/minecolonies_tweaks/core/common/MineColoniesTweaks.java
@@ -3,9 +3,11 @@ package steve_gall.minecolonies_tweaks.core.common;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.minecolonies.api.colony.buildings.ModBuildings;
 import com.minecolonies.api.colony.requestsystem.StandardFactoryController;
 import com.minecolonies.api.colony.requestsystem.manager.RequestMappingHandler;
 import com.minecolonies.api.items.ModItems;
+import com.minecolonies.core.colony.buildings.modules.BuildingModules;
 
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.resources.ResourceLocation;
@@ -82,6 +84,7 @@ public class MineColoniesTweaks
 			StandardFactoryController.getInstance().registerNewFactory(new CustomizableDeliverableRequestFactory());
 
 			CustomCraftingModule.loadCustomCraftingModules();
+			ModBuildings.postBox.get().getModuleProducers().add(BuildingModules.MIN_STOCK);
 
 			DispenserBlock.registerBehavior(ModItems.compost, new CompostDispenseItemBehavior());
 
