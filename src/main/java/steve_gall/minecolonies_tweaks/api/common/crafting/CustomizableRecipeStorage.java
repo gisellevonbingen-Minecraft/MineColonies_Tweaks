27458c82c320e69b69f4d0dcb04fa994e783f457
Giselle <gisellevonbingen@gmail.com>
2025-04-09 21:44:16 +0900

    Delete DelegateRecipeStorage.java

--- src/main/java/steve_gall/minecolonies_tweaks/api/common/crafting/CustomizableRecipeStorage.java
+++ src/main/java/steve_gall/minecolonies_tweaks/api/common/crafting/CustomizableRecipeStorage.java
@@ -24,13 +24,14 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraftforge.items.IItemHandler;
+import steve_gall.minecolonies_tweaks.core.common.crafting.RecipeStorageExtension;
 
 public class CustomizableRecipeStorage implements ICustomizableRecipeStorage
 {
 	@NotNull
 	private final ICustomizedRecipeStorage impl;
 	@NotNull
-	private final DelegateRecipeStorage delegate;
+	private final RecipeStorage delegate;
 	@NotNull
 	private final AbstractRecipeType<IRecipeStorage> recipeType;
 
@@ -40,7 +41,20 @@ public class CustomizableRecipeStorage implements ICustomizableRecipeStorage
 	public CustomizableRecipeStorage(@NotNull IToken<?> token, @NotNull ICustomizedRecipeStorage impl)
 	{
 		this.impl = impl;
-		this.delegate = new DelegateRecipeStorage(this, token, impl.getInput(), impl.getGridSize(), impl.getPrimaryOutput(), impl.getIntermediate(), impl.getRecipeSource(), impl.getRecipeType(), impl.getAlternateOutputs(), getSecondaryOutputs(impl), impl.getLootTable(), impl.getRequiredTool());
+		this.delegate = RecipeStorage.builder()//
+				.withToken(token)//
+				.withInputs(impl.getInput())//
+				.withGridSize(impl.getGridSize())//
+				.withPrimaryOutput(impl.getPrimaryOutput())//
+				.withIntermediate(impl.getIntermediate())//
+				.withRecipeId(impl.getRecipeSource())//
+				.withRecipeType(impl.getRecipeType())//
+				.withAlternateOutputs(impl.getAlternateOutputs())//
+				.withSecondaryOutputs(getSecondaryOutputs(impl))//
+				.withLootTable(impl.getLootTable())//
+				.withRequiredTool(impl.getRequiredTool()//
+				).build();
+		((RecipeStorageExtension) this.delegate).minecolonies_tweaks$setCustomized(impl);
 
 		var type = impl.getRecipeType();
 		var recipeTypes = MinecoloniesAPIProxy.getInstance().getRecipeTypeRegistry();
