808ff2a3abc34262fcf67c2dc36b5cd0872080f4
Giselle <gisellevonbingen@gmail.com>
2025-04-19 12:38:18 +0900

    TamedAnimal don't attack citizen.

--- src/main/java/steve_gall/minecolonies_tweaks/core/common/CommonForgeEventHandler.java
+++ src/main/java/steve_gall/minecolonies_tweaks/core/common/CommonForgeEventHandler.java
@@ -4,10 +4,14 @@ import java.util.HashMap;
 
 import com.minecolonies.api.colony.IColonyManager;
 import com.minecolonies.api.util.constant.TranslationConstants;
+import com.minecolonies.core.entity.citizen.EntityCitizen;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.living.LivingChangeTargetEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -42,6 +46,36 @@ public class CommonForgeEventHandler
 
 	}
 
+	@SubscribeEvent
+	public void onLivingChangeTarget(LivingChangeTargetEvent e)
+	{
+		if (e.getEntity() instanceof TamableAnimal animal && animal.getOwner() instanceof Player player)
+		{
+			if (e.getNewTarget() instanceof EntityCitizen citizen)
+			{
+				var citizenColonyHandler = citizen.getCitizenColonyHandler();
+
+				if (citizenColonyHandler != null)
+				{
+					var colony = citizenColonyHandler.getColony();
+
+					if (colony != null)
+					{
+						if (colony.getPermissions().isColonyMember(player))
+						{
+							e.setCanceled(true);
+						}
+
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+	
 	@SubscribeEvent
 	public void onResearchEffectChangedEvent(ResearchEffectChangedEventArgs e)
 	{
