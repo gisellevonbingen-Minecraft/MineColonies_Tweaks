989af314524b89d3e9e3feda03eaa8c82ae4f894
Giselle <gisellevonbingen@gmail.com>
2025-04-22 23:52:22 +0900

    Food

--- src/main/java/steve_gall/minecolonies_tweaks/mixin/common/minecolonies/FoodUtilsMixin.java
+++ src/main/java/steve_gall/minecolonies_tweaks/mixin/common/minecolonies/FoodUtilsMixin.java
@@ -4,13 +4,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import com.minecolonies.api.items.IMinecoloniesFoodItem;
-import com.minecolonies.api.items.ModItems;
+import com.minecolonies.api.crafting.ItemStorage;
 import com.minecolonies.api.util.FoodUtils;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import steve_gall.minecolonies_tweaks.core.common.init.MCTweaksTags;
+import steve_gall.minecolonies_tweaks.core.common.item.FoodUtils2;
 
 @Mixin(value = FoodUtils.class, remap = false)
 public abstract class FoodUtilsMixin
@@ -18,32 +17,31 @@ public abstract class FoodUtilsMixin
 	@Redirect(method = "getFoodValue(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/food/FoodProperties;D)D", remap = false, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;", remap = true))
 	private static Item getFoodValue(ItemStack foodStack)
 	{
-		var item = foodStack.getItem();
+		return FoodUtils2.getTierRepresentedFood(foodStack);
+	}
 
-		if (item instanceof IMinecoloniesFoodItem)
-		{
-			return item;
-		}
+	@Redirect(method = "getBestFoodForCitizen", remap = false, at = @At(value = "INVOKE", target = "Lcom/minecolonies/api/crafting/ItemStorage;getItem()Lnet/minecraft/world/item/Item;", remap = false, ordinal = 0))
+	private static Item getBestFoodForCitizen0(ItemStorage foodStack)
+	{
+		return FoodUtils2.getTierRepresentedFood(foodStack.getItemStack());
+	}
 
-		var tier = MCTweaksTags.Items.getFoodTier(foodStack::is);
+	@Redirect(method = "getBestFoodForCitizen", remap = false, at = @At(value = "INVOKE", target = "Lcom/minecolonies/api/crafting/ItemStorage;getItem()Lnet/minecraft/world/item/Item;", remap = false, ordinal = 3))
+	private static Item getBestFoodForCitizen3(ItemStorage foodStack)
+	{
+		return FoodUtils2.getTierRepresentedFood(foodStack.getItemStack());
+	}
 
-		if (tier == 3)
-		{
-			return ModItems.hand_pie;
-		}
-		else if (tier == 2)
-		{
-			return ModItems.manchet_bread;
-		}
-		else if (tier == 1)
-		{
-			return ModItems.cheddar_cheese;
-		}
-		else
-		{
-			return item;
-		}
+	@Redirect(method = "checkForFoodInBuilding", remap = false, at = @At(value = "INVOKE", target = "Lcom/minecolonies/api/crafting/ItemStorage;getItem()Lnet/minecraft/world/item/Item;", remap = false, ordinal = 0))
+	private static Item checkForFoodInBuilding0(ItemStorage foodStack)
+	{
+		return FoodUtils2.getTierRepresentedFood(foodStack.getItemStack());
+	}
 
+	@Redirect(method = "checkForFoodInBuilding", remap = false, at = @At(value = "INVOKE", target = "Lcom/minecolonies/api/crafting/ItemStorage;getItem()Lnet/minecraft/world/item/Item;", remap = false, ordinal = 2))
+	private static Item checkForFoodInBuilding2(ItemStorage foodStack)
+	{
+		return FoodUtils2.getTierRepresentedFood(foodStack.getItemStack());
 	}
 
 }
