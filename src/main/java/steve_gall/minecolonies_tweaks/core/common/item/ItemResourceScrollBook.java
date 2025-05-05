30ba05c1c294856c411ad2c2f2aeb308821105c3
Giselle <gisellevonbingen@gmail.com>
2025-05-05 21:13:22 +0900

    Update ItemResourceScrollBook.java

--- src/main/java/steve_gall/minecolonies_tweaks/core/common/item/ItemResourceScrollBook.java
+++ src/main/java/steve_gall/minecolonies_tweaks/core/common/item/ItemResourceScrollBook.java
@@ -114,7 +114,7 @@ public class ItemResourceScrollBook extends Item
 			}
 
 		}
-		else
+		else if (!player.isShiftKeyDown())
 		{
 			this.openWindow(stack);
 		}
@@ -135,7 +135,7 @@ public class ItemResourceScrollBook extends Item
 			}
 
 		}
-		else
+		else if (!player.isShiftKeyDown())
 		{
 			this.openWindow(stack);
 		}
