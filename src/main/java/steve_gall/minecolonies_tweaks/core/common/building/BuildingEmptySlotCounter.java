9cb37b6cf9d039aae282e0a8e1431cb51adcff6d
Giselle <gisellevonbingen@gmail.com>
2025-05-05 22:15:03 +0900

    BuildingEmptySlotCounter

---
+++ src/main/java/steve_gall/minecolonies_tweaks/core/common/building/BuildingEmptySlotCounter.java
@@ -0,0 +1,163 @@
+package steve_gall.minecolonies_tweaks.core.common.building;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.minecolonies.api.colony.buildings.views.IBuildingView;
+import com.minecolonies.api.tileentities.AbstractTileEntityRack;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.items.IItemHandler;
+
+public class BuildingEmptySlotCounter
+{
+	private static final DecimalFormat SLOTS_FORMAT = new DecimalFormat("#,###");
+	private static final DecimalFormat PERCENT_FORMAT = new DecimalFormat("0.0");
+
+	private static final int EMPTY_SLOTS_UPDATE_TICKS = 20;
+
+	private final IBuildingView buildingView;
+
+	private boolean first = true;
+	private List<BlockPos> positions = null;
+	private int ticks = 0;
+	private int emptySlots = 0;
+	private int totalSlots = 0;
+
+	public BuildingEmptySlotCounter(IBuildingView buildingView)
+	{
+		this.buildingView = buildingView;
+	}
+
+	public boolean update()
+	{
+		if (this.first)
+		{
+			this.first = false;
+			this.updatePositions();
+
+			for (var position : this.positions)
+			{
+				this.countUp(position);
+			}
+
+			return true;
+		}
+		else
+		{
+			if (this.ticks == 0)
+			{
+				this.emptySlots = 0;
+				this.totalSlots = 0;
+				this.updatePositions();
+			}
+
+			var containerCount = this.positions.size();
+			var count = (containerCount + EMPTY_SLOTS_UPDATE_TICKS - 1) / EMPTY_SLOTS_UPDATE_TICKS;
+			var offset = this.ticks * count;
+			var endIndex = Math.min(offset + count, containerCount);
+
+			for (var i = offset; i < endIndex; i++)
+			{
+				this.countUp(this.positions.get(i));
+			}
+
+			this.ticks++;
+
+			if (this.ticks >= EMPTY_SLOTS_UPDATE_TICKS)
+			{
+				this.ticks = 0;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+
+	}
+
+	private void updatePositions()
+	{
+		var positions = new ArrayList<BlockPos>();
+		positions.add(buildingView.getPosition());
+		positions.addAll(buildingView.getContainerList());
+		this.positions = positions;
+	}
+
+	private void countUp(BlockPos position)
+	{
+		var level = this.buildingView.getColony().getWorld();
+		var blockEntity = level.getBlockEntity(position);
+		var itemHandler = this.getItemHandler(blockEntity);
+
+		if (itemHandler == null)
+		{
+			return;
+		}
+
+		var slots = itemHandler.getSlots();
+
+		for (var i = 0; i < slots; i++)
+		{
+			if (itemHandler.getStackInSlot(i).isEmpty())
+			{
+				this.emptySlots++;
+			}
+
+			this.totalSlots++;
+		}
+
+	}
+
+	private IItemHandler getItemHandler(BlockEntity blockEntity)
+	{
+		if (blockEntity == null)
+		{
+			return null;
+		}
+		else if (blockEntity instanceof AbstractTileEntityRack rack)
+		{
+			return rack.getInventory();
+		}
+		else
+		{
+			var cap = blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER);
+			return cap == null ? null : cap.orElse(null);
+		}
+
+	}
+
+	public IBuildingView getBuildingView()
+	{
+		return this.buildingView;
+	}
+
+	public int getEmptySlots()
+	{
+		return this.emptySlots;
+	}
+
+	public int getTotalSlots()
+	{
+		return this.totalSlots;
+	}
+
+	public Component getSlotsText()
+	{
+		var s1 = SLOTS_FORMAT.format(this.emptySlots);
+		var s2 = SLOTS_FORMAT.format(this.totalSlots);
+		return Component.translatable("minecolonies_tweaks.gui.warehouse.empty_slots", s1, s2);
+	}
+
+	public String getPercentText()
+	{
+		return PERCENT_FORMAT.format(this.emptySlots / (this.totalSlots / 100.0F)) + "%";
+	}
+
+}
--- src/main/java/steve_gall/minecolonies_tweaks/mixin/client/minecolonies/AbstractWindowModuleBuildingMixin.java
+++ src/main/java/steve_gall/minecolonies_tweaks/mixin/client/minecolonies/AbstractWindowModuleBuildingMixin.java
@@ -1,9 +1,5 @@
 package steve_gall.minecolonies_tweaks.mixin.client.minecolonies;
 
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,37 +9,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import com.ldtteam.blockui.Loader;
 import com.ldtteam.blockui.controls.Text;
 import com.minecolonies.api.colony.buildings.views.IBuildingView;
-import com.minecolonies.api.tileentities.AbstractTileEntityRack;
 import com.minecolonies.core.client.gui.AbstractModuleWindow;
 import com.minecolonies.core.client.gui.AbstractWindowModuleBuilding;
 import com.minecolonies.core.colony.buildings.workerbuildings.BuildingWareHouse;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraftforge.common.capabilities.ForgeCapabilities;
-import net.minecraftforge.items.IItemHandler;
 import steve_gall.minecolonies_tweaks.core.common.MineColoniesTweaks;
+import steve_gall.minecolonies_tweaks.core.common.building.BuildingEmptySlotCounter;
 
 @Mixin(value = AbstractWindowModuleBuilding.class, remap = false)
 public abstract class AbstractWindowModuleBuildingMixin<B extends IBuildingView> extends AbstractModuleWindow
 {
 	private static final String TEXT_EMPTY_SLOTS = "empty_slots";
-	private static final DecimalFormat SLOTS_FORMAT = new DecimalFormat("#,###");
-	private static final DecimalFormat PERCENT_FORMAT = new DecimalFormat("0.0");
-
-	private static final int EMPTY_SLOTS_UPDATE_TICKS = 20;
 
 	@Unique
-	private boolean minecolonies_tweaks$first = true;
-	@Unique
-	private int minecolonies_tweaks$updateTicks;
-	@Unique
-	private List<BlockPos> minecolonies_tweaks$containerPositions;
-	@Unique
-	private int minecolonies_tweaks$emptySlots;
-	@Unique
-	private int minecolonies_tweaks$totalSlots;
+	private BuildingEmptySlotCounter minecolonies_tweaks$slotCounter;
 
 	public AbstractWindowModuleBuildingMixin(IBuildingView building, String res)
 	{
@@ -65,44 +45,14 @@ public abstract class AbstractWindowModuleBuildingMixin<B extends IBuildingView>
 	{
 		if (this.buildingView instanceof BuildingWareHouse.View)
 		{
-			if (this.minecolonies_tweaks$first)
+			if (this.minecolonies_tweaks$slotCounter == null)
 			{
-				this.minecolonies_tweaks$first = false;
-
-				for (var position : this.getContainerPositions())
-				{
-					this.countUp(position);
-				}
-
-				this.updateText();
+				this.minecolonies_tweaks$slotCounter = new BuildingEmptySlotCounter(this.buildingView);
 			}
-			else
+
+			if (this.minecolonies_tweaks$slotCounter.update())
 			{
-				if (this.minecolonies_tweaks$updateTicks == 0)
-				{
-					this.minecolonies_tweaks$emptySlots = 0;
-					this.minecolonies_tweaks$totalSlots = 0;
-					this.minecolonies_tweaks$containerPositions = this.getContainerPositions();
-				}
-
-				var containerCount = this.minecolonies_tweaks$containerPositions.size();
-				var count = (containerCount + EMPTY_SLOTS_UPDATE_TICKS - 1) / EMPTY_SLOTS_UPDATE_TICKS;
-				var offset = this.minecolonies_tweaks$updateTicks * count;
-				var endIndex = Math.min(offset + count, containerCount);
-
-				for (var i = offset; i < endIndex; i++)
-				{
-					this.countUp(this.minecolonies_tweaks$containerPositions.get(i));
-				}
-
-				this.minecolonies_tweaks$updateTicks++;
-
-				if (this.minecolonies_tweaks$updateTicks >= EMPTY_SLOTS_UPDATE_TICKS)
-				{
-					this.updateText();
-					this.minecolonies_tweaks$updateTicks = 0;
-				}
-
+				this.updateText();
 			}
 
 		}
@@ -111,63 +61,11 @@ public abstract class AbstractWindowModuleBuildingMixin<B extends IBuildingView>
 
 	private void updateText()
 	{
-		var text = this.findPaneOfTypeByID(TEXT_EMPTY_SLOTS, Text.class);
-		var s1 = SLOTS_FORMAT.format(this.minecolonies_tweaks$emptySlots);
-		var s2 = SLOTS_FORMAT.format(this.minecolonies_tweaks$totalSlots);
-		var s3 = PERCENT_FORMAT.format(this.minecolonies_tweaks$emptySlots / (this.minecolonies_tweaks$totalSlots / 100.0F)) + "%";
-		text.setText(Component.translatable("minecolonies_tweaks.gui.warehouse.empty_slots", s1, s2, s3));
-	}
-
-	private List<BlockPos> getContainerPositions()
-	{
-		var positions = new ArrayList<BlockPos>();
-		positions.add(this.buildingView.getPosition());
-		positions.addAll(this.buildingView.getContainerList());
-		return positions;
-	}
-
-	private void countUp(BlockPos position)
-	{
-		var level = this.buildingView.getColony().getWorld();
-
-		var blockEntity = level.getBlockEntity(position);
-		var itemHandler = this.getItemHandler(blockEntity);
-
-		if (itemHandler == null)
-		{
-			return;
-		}
-
-		var slots = itemHandler.getSlots();
-
-		for (var i = 0; i < slots; i++)
-		{
-			if (itemHandler.getStackInSlot(i).isEmpty())
-			{
-				this.minecolonies_tweaks$emptySlots++;
-			}
-
-			this.minecolonies_tweaks$totalSlots++;
-		}
-
-	}
-
-	private IItemHandler getItemHandler(BlockEntity blockEntity)
-	{
-		if (blockEntity == null)
-		{
-			return null;
-		}
-		else if (blockEntity instanceof AbstractTileEntityRack rack)
-		{
-			return rack.getInventory();
-		}
-		else
-		{
-			var cap = blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER);
-			return cap == null ? null : cap.orElse(null);
-		}
-
+		var slotsText = this.findPaneOfTypeByID(TEXT_EMPTY_SLOTS, Text.class);
+		var text = Component.empty();
+		text.append(this.minecolonies_tweaks$slotCounter.getSlotsText());
+		text.append(" (" + this.minecolonies_tweaks$slotCounter.getPercentText() + ")");
+		slotsText.setText(text);
 	}
 
 }
