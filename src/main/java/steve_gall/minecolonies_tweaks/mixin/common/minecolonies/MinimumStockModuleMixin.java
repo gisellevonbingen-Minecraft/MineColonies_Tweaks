ac0fc47e97069a2c00c497dd4c7bc8ae6695ff50
Giselle <gisellevonbingen@gmail.com>
2025-05-08 15:33:12 +0900

    MixinExtra

--- src/main/java/steve_gall/minecolonies_tweaks/mixin/common/minecolonies/MinimumStockModuleMixin.java
+++ src/main/java/steve_gall/minecolonies_tweaks/mixin/common/minecolonies/MinimumStockModuleMixin.java
@@ -2,8 +2,9 @@ package steve_gall.minecolonies_tweaks.mixin.common.minecolonies;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.minecolonies.api.colony.buildings.IBuilding;
 import com.minecolonies.core.colony.buildings.modules.MinimumStockModule;
 import com.minecolonies.core.colony.buildings.workerbuildings.PostBox;
@@ -11,8 +12,8 @@ import com.minecolonies.core.colony.buildings.workerbuildings.PostBox;
 @Mixin(value = MinimumStockModule.class, remap = false)
 public abstract class MinimumStockModuleMixin
 {
-	@Redirect(method = "minimumStockSize", remap = false, at = @At(value = "INVOKE", target = "Lcom/minecolonies/api/colony/buildings/IBuilding;getBuildingLevel()I", remap = false))
-	private int minimumStockSize(IBuilding building)
+	@WrapOperation(method = "minimumStockSize", remap = false, at = @At(value = "INVOKE", target = "Lcom/minecolonies/api/colony/buildings/IBuilding;getBuildingLevel()I", remap = false))
+	private int minimumStockSize(IBuilding building, Operation<Integer> operation)
 	{
 		if (building instanceof PostBox)
 		{
@@ -21,7 +22,7 @@ public abstract class MinimumStockModuleMixin
 		}
 		else
 		{
-			return building.getBuildingLevel();
+			return operation.call(building);
 		}
 
 	}
