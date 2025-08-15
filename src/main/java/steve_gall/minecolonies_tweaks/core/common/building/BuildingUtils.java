0260afbcec53e42943c8e39e60bc59c658e45b38
Giselle <gisellevonbingen@gmail.com>
2025-08-16 00:20:35 +0900

    getRequesterDisplayName

--- src/main/java/steve_gall/minecolonies_tweaks/core/common/building/BuildingUtils.java
+++ src/main/java/steve_gall/minecolonies_tweaks/core/common/building/BuildingUtils.java
@@ -1,14 +1,18 @@
 package steve_gall.minecolonies_tweaks.core.common.building;
 
+import org.jetbrains.annotations.Nullable;
+
 import com.minecolonies.api.colony.buildings.IBuilding;
 import com.minecolonies.api.colony.buildings.registry.BuildingEntry;
 import com.minecolonies.api.colony.buildings.views.IBuildingView;
+import com.minecolonies.api.colony.requestsystem.requester.IRequester;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 
 public class BuildingUtils
 {
-	private static Component getDisplayName(BuildingEntry type, String customName, int level)
+	private static MutableComponent getDisplayName(BuildingEntry type, String customName, int level)
 	{
 		var buildingName = Component.empty();
 		buildingName.append(customName.isEmpty() ? Component.translatable(type.getTranslationKey()) : Component.literal(customName));
@@ -16,12 +20,30 @@ public class BuildingUtils
 		return buildingName;
 	}
 
-	public static Component getDisplayName(IBuilding building)
+	@Nullable
+	public static MutableComponent getDisplayName(IRequester requester)
+	{
+		if (requester instanceof IBuilding building)
+		{
+			return getDisplayName(building);
+		}
+		else if (requester instanceof IBuildingView buildingView)
+		{
+			return getDisplayName(buildingView);
+		}
+		else
+		{
+			return null;
+		}
+
+	}
+
+	public static MutableComponent getDisplayName(IBuilding building)
 	{
 		return getDisplayName(building.getBuildingType(), building.getCustomName(), building.getBuildingLevel());
 	}
 
-	public static Component getDisplayName(IBuildingView buildingView)
+	public static MutableComponent getDisplayName(IBuildingView buildingView)
 	{
 		return getDisplayName(buildingView.getBuildingType(), buildingView.getCustomName(), buildingView.getBuildingLevel());
 	}
