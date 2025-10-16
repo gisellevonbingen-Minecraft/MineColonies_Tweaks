ba1229fddf6295453df1348d3935e2d743c88f49
Giselle <gisellevonbingen@gmail.com>
2025-10-16 23:39:43 +0900

    AbstractIdListModule

--- src/main/java/steve_gall/minecolonies_tweaks/api/common/building/module/AbstractIdListModuleView.java
+++ src/main/java/steve_gall/minecolonies_tweaks/api/common/building/module/AbstractIdListModuleView.java
@@ -37,10 +37,10 @@ public abstract class AbstractIdListModuleView extends AbstractBuildingModuleVie
 	}
 
 	@Override
-	public void addIds(@NotNull Collection<ResourceLocation> ids)
+	public boolean addIds(@NotNull Collection<ResourceLocation> ids)
 	{
 		PacketDistributor.sendToServer(new AssignIdListMessage(this, Function.ADD, ids));
-		this.ids.addAll(ids);
+		return this.ids.addAll(ids);
 	}
 
 	@Override
