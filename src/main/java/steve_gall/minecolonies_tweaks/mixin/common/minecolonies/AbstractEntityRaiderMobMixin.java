7731584cf0532acb16cf18bf7896c1b42434bfa9
Giselle <gisellevonbingen@gmail.com>
2025-11-25 22:37:56 +0900

    Update AbstractEntityRaiderMobMixin.java

--- src/main/java/steve_gall/minecolonies_tweaks/mixin/common/minecolonies/AbstractEntityRaiderMobMixin.java
+++ src/main/java/steve_gall/minecolonies_tweaks/mixin/common/minecolonies/AbstractEntityRaiderMobMixin.java
@@ -6,60 +6,31 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.minecolonies.api.entity.mobs.AbstractEntityRaiderMob;
 
+import net.minecraft.world.damagesource.DamageSource;
 import steve_gall.minecolonies_tweaks.core.common.config.MCTweaksConfigServer;
 
 @Mixin(value = AbstractEntityRaiderMob.class, remap = false)
 public abstract class AbstractEntityRaiderMobMixin
 {
 	@Shadow(remap = false)
-	private int envDamageInterval;
+	private int envDmgCooldown;
 	@Shadow(remap = false)
 	private boolean envDamageImmunity;
 	@Shadow(remap = false)
 	private boolean tempEnvDamageImmunity;
 
-	@Inject(method = "initStatsFor", remap = false, at = @At(value = "HEAD"), cancellable = false)
-	private void initStatsFor(double baseHealth, double difficulty, double baseDamage, CallbackInfo ci)
+	@Inject(method = "hurt", remap = false, at = @At(value = "HEAD"), cancellable = false)
+	private void hurt(DamageSource damageSource, float damage, CallbackInfoReturnable<Boolean> cir)
 	{
 		if (MCTweaksConfigServer.INSTANCE.monsters.disableImmunity.get())
 		{
-			this.envDamageInterval = 0;
+			this.envDmgCooldown = 0;
 			this.envDamageImmunity = false;
 			this.tempEnvDamageImmunity = false;
-		}
-
-	}
-
-	@Inject(method = "setEnvDamageInterval", remap = false, at = @At(value = "HEAD"), cancellable = true)
-	private void setEnvDamageInterval(int interval, CallbackInfo ci)
-	{
-		if (interval > 0 && MCTweaksConfigServer.INSTANCE.monsters.disableImmunity.get())
-		{
-			ci.cancel();
-		}
-
-	}
-
-	@Inject(method = "setEnvDamageImmunity", remap = false, at = @At(value = "HEAD"), cancellable = true)
-	private void setEnvDamageImmunity(boolean immunity, CallbackInfo ci)
-	{
-		if (immunity && MCTweaksConfigServer.INSTANCE.monsters.disableImmunity.get())
-		{
-			ci.cancel();
-		}
-
-	}
-
-	@Inject(method = "setTempEnvDamageImmunity", remap = false, at = @At(value = "HEAD"), cancellable = true)
-	private void setTempEnvDamageImmunity(boolean immunity, CallbackInfo ci)
-	{
-		if (immunity && MCTweaksConfigServer.INSTANCE.monsters.disableImmunity.get())
-		{
-			ci.cancel();
 		}
 
 	}
