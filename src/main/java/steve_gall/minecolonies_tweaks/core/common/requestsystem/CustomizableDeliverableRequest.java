136ffa19c3a71d34e432583ddec3e01dcb0bb88f
Giselle <gisellevonbingen@gmail.com>
2025-11-30 11:39:57 +0900

    Show request count on display string

--- src/main/java/steve_gall/minecolonies_tweaks/core/common/requestsystem/CustomizableDeliverableRequest.java
+++ src/main/java/steve_gall/minecolonies_tweaks/core/common/requestsystem/CustomizableDeliverableRequest.java
@@ -6,6 +6,7 @@ import com.minecolonies.api.colony.requestsystem.request.RequestState;
 import com.minecolonies.api.colony.requestsystem.requester.IRequester;
 import com.minecolonies.api.colony.requestsystem.token.IToken;
 
+import net.minecraft.network.chat.Component;
 import steve_gall.minecolonies_tweaks.api.common.requestsystem.CustomizableDeliverable;
 
 public class CustomizableDeliverableRequest extends CustomizableRequest<CustomizableDeliverable>
@@ -18,6 +19,34 @@ public class CustomizableDeliverableRequest extends CustomizableRequest<Customiz
 	public CustomizableDeliverableRequest(@NotNull IRequester requester, @NotNull IToken<?> token, @NotNull RequestState state, @NotNull CustomizableDeliverable requested)
 	{
 		super(requester, token, state, requested);
+	}
+
+	@Override
+	public @NotNull Component getShortDisplayString()
+	{
+		return getDisplayString(super.getShortDisplayString());
+	}
+
+	@Override
+	public @NotNull Component getLongDisplayString()
+	{
+		return getDisplayString(super.getLongDisplayString());
+	}
+
+	protected @NotNull Component getDisplayString(Component displayString)
+	{
+		var min = this.getRequest().getMinimumCount();
+		var count = this.getRequest().getCount();
+
+		if (min == count)
+		{
+			return Component.translatable("%s %s", count, displayString);
+		}
+		else
+		{
+			return Component.translatable("%s-%s %s", min, count, displayString);
+		}
+
 	}
 
 }
