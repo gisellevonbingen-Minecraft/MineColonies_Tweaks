0e1eb65778e38abfaa74edbff6c6a51913509226
Giselle <gisellevonbingen@gmail.com>
2025-12-15 23:40:02 +0900

    Show building level

--- src/main/java/steve_gall/minecolonies_tweaks/core/client/gui/ResourceScrollElement.java
+++ src/main/java/steve_gall/minecolonies_tweaks/core/client/gui/ResourceScrollElement.java
@@ -51,7 +51,7 @@ public class ResourceScrollElement extends ResourceScrollBookElement
 		}
 
 		this.valid = true;
-		this.workerName = Component.literal(buildingView.getWorkerName()).withStyle(ChatFormatting.DARK_PURPLE);
+		this.workerName = Component.translatable("%s (lvl. %s)", buildingView.getWorkerName(), buildingView.getBuildingLevel()).withStyle(ChatFormatting.DARK_PURPLE);
 
 		var resourceView = buildingView.getModuleViewByType(BuildingResourcesModuleView.class);
 		var workOrderView = buildingView.getColony().getWorkOrder(resourceView.getWorkOrderId());
