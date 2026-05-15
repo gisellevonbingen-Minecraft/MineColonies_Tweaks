f16a52038e898f105d8159a17bf918ae0ff2cb88
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2026-05-16 00:54:32 +0900

    implements Clearable #91

---
+++ src/main/java/steve_gall/minecolonies_tweaks/mixin/common/minecolonies/AbstractTileEntityRackMixin.java
@@ -0,0 +1,30 @@
+package steve_gall.minecolonies_tweaks.mixin.common.minecolonies;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import com.minecolonies.api.tileentities.AbstractTileEntityRack;
+
+import net.minecraft.world.Clearable;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.items.ItemStackHandler;
+
+@Mixin(value = AbstractTileEntityRack.class, remap = false)
+public abstract class AbstractTileEntityRackMixin implements Clearable
+{
+	@Shadow(remap = false)
+	private ItemStackHandler inventory;
+
+	@Override
+	public void clearContent()
+	{
+		var inventory = this.inventory;
+
+		for (var i = 0; i < inventory.getSlots(); i++)
+		{
+			inventory.setStackInSlot(i, ItemStack.EMPTY);
+		}
+
+	}
+
+}
